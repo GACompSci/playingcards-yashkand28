6a8a6542f902ceaa7e62602f5d212b00fa55cace
Yash Kandanuru <ykand25@germantownacademy.org>
2024-12-10 14:13:03 -0500

    Completed Card Project

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // Use nested loops to create a full deck (plus two Jokers)
-        // printing each card as you create it. 
+        for (int suit = 1; suit <= 4; suit++){
+            for (int value = 1; value <= 13; value++){
+                Card LeCard = new Card(value, suit);
+                System.out.println(LeCard); 
+            }
+        }
+
+        Card LeJoker1 = new Card(); 
+        Card LeJoker2 = new Card();
+        System.out.println(LeJoker1);
+        System.out.println(LeJoker2);
     }
 }
