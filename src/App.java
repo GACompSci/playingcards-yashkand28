8b872e05ca3a8aa31c0c87c0e4636a800372639b
Yash Kandanuru <ykand25@germantownacademy.org>
2025-01-21 13:30:28 -0500

    Deck Class

--- src/App.java
+++ src/App.java
@@ -1,15 +1,13 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        for (int suit = 1; suit <= 4; suit++){
-            for (int value = 1; value <= 13; value++){
-                Card LeCard = new Card(value, suit);
-                System.out.println(LeCard); 
-            }
+    public static void main(String[] args) throws Exception { 
+        Deck deck = new Deck();
+
+        for (Card c : deck.getDeck()){
+            System.out.println(c);
         }
 
-        Card LeJoker1 = new Card(); 
-        Card LeJoker2 = new Card();
-        System.out.println(LeJoker1);
-        System.out.println(LeJoker2);
+        System.out.println("The first drawn card is: " + deck.drawCard());
+        System.out.println("The second drawn card is: " + deck.drawCard());
+        System.out.println(deck);
     }
 }
